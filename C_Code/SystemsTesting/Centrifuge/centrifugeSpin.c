b0432bf12c7be97ab3288be6f9770bab36c2629a
HR-pi-Admin <instrumentationsubsystem@gmail.com>
2026-05-21 18:24:01 -0700

    dirtSample changes

--- C_Code/SystemsTesting/Centrifuge/centrifugeSpin.c
+++ C_Code/SystemsTesting/Centrifuge/centrifugeSpin.c
@@ -10,20 +10,80 @@
 #include <wiringPiSPI.h>
 // #include <signal.h>
 #define SPIN_CHANNEL LOAD12_3 // Centrifuge spin pin
-#define period_duty 10
+#define SERVO_CHANNEL CENTRIFUGE_SERVO_CHANNEL      // FPGA PWM channel for the centrifuge servo
+#define SERVO_PERIOD 10000                      // full duty (on)
+#define SERVO_OFF 0                             // off
+// #define CPR 1018                                // Counts Per Revolution of the absolute encoder
+#define ENC_ABS ENC_CENTRIFUGE_ABS
 
 static volatile int sigint = 0;
 
 static void intHandler(int dummy) {
   (void)dummy;
   sigint = 1;
-  digitalWrite(LEFTEN, 0);
+  digitalWrite(SPIN_CHANNEL, 0);
 }
 
 static void spin_off(void) {
-  digitalWrite(LEFTEN, 0);
+  digitalWrite(SPIN_CHANNEL, 0);
 }
 
+static void pwm_off(void) { 
+  fpga_pwm_uptime(SERVO_CHANNEL, SERVO_OFF); 
+}
+
+void rotateTo(int target_pos) {
+  int current = fpga_safetran(ENC_ABS);
+
+  printf("Current: %i, Target: %i\n", current, target_pos);
+
+  // Turn on the centrifuge motor
+  fpga_pwm_uptime(SERVO_CHANNEL, SERVO_PERIOD);
+  int distance_remaining = abs(target_pos - current);
+
+  // Wait until the target position is reached, 10 units before start slowing down
+  while (distance_remaining > 5) {
+    if (sigint) {
+      break;
+    }
+    usleep(1E3);
+    current = fpga_safetran(ENC_ABS);
+    distance_remaining = abs(target_pos - current);
+  }
+
+  // Turn off the centrifuge motor
+  pwm_off();
+
+  current = fpga_safetran(ENC_ABS);
+  printf("Final: %i\n", current);
+}
+
+void softPWM_ramp(int pin, int period_scale) {
+  printf("Period: %i pin: %i\n", period_scale, pin);
+  for(int i = 0; i <= period_scale; i++) {
+    digitalWrite(pin, 1);
+    usleep(i * 100);
+    digitalWrite(pin, 0);
+    usleep((period_scale - i) * 100);
+  }
+}
+
+// duration_seconds does not account for time taken to ramp up
 void spinFor(int duration_seconds) {
-  
+  int start_pos = fpga_safetran(ENC_ABS);
+  printf("Spinning for %d seconds\n", duration_seconds);
+  softPWM_ramp(SPIN_CHANNEL, 100);
+  digitalWrite(SPIN_CHANNEL, 1);
+  sleep(duration_seconds);
+  spin_off();
+  printf("Done spinning; rotating to initial position: %d\n", start_pos);
+  rotateTo(start_pos);
+}
+
+int main(int argc, char* argv[]) {
+  signal(SIGINT, intHandler);
+  wiringPiSetupPinType(WPI_PIN_WPI);
+  int vals[argc - 1];
+  intparse(argc - 1, argv + 1, vals);
+  spinFor(vals[0]);
 }
