79d79f49f766d76252592688f4da88a2ea9952b1
HR-pi-Admin <instrumentationsubsystem@gmail.com>
2026-05-24 20:06:40 -0700

    add executables

--- Executables/startup.sh
+++ Executables/startup.sh
@@ -4,6 +4,6 @@ source /home/robot/HR-pi/Executables/variables.sh
 /home/robot/HR-pi/Executables/all_low
 /home/robot/HR-pi/Executables/toggle $FAN1
 /home/robot/HR-pi/Executables/toggle $FAN2
-/home/robot/HR-pi/Executables/fpwm 2 2500 20000 # Dirt sample motor
+/home/robot/HR-pi/Executables/fpwm 2 2320 20000 # Dirt sample motor
 /home/robot/HR-pi/Executables/fpwm 1 1500 20000 # Auger
 /home/robot/HR-pi/Executables/fpwm 0 0 20000 # Centrifuge servo
